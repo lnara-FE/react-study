3c4968b3eb5b8c8c02d93502c7b82280ca76ff65
lnara-FE <naas0974@gmail.com>
2022-04-26 16:16:43 +0900

    feat : Add counter

--- react-test-app/src/App.js
+++ react-test-app/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [disabled, setDisabled ] = useState(false);
   return (
-    <div className="App">
-      react-test-app
+    <div className='App'>
+      <header className='App-header'>
+        <h3 data-testid='counter'>{counter}</h3>
+        <div>
+          <button onClick={() => setCounter((count) => count - 1)} disabled={disabled} data-testid='minus-button'>-</button>
+          <button onClick={() => setCounter((count) => count + 1)} disabled={disabled} data-testid='plus-button'>+</button>
+          <button onClick={() => setDisabled((prev) => !prev)} style={{backgroundColor: 'blue'}} data-testid='on/off-button'>on/off</button>
+        </div>
+      </header>
     </div>
   );
 }
